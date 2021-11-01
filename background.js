d2c34c1a4ccb4fe8b82a24d6040904cc4d48e71e
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2021-11-01 20:49:54 +0000

    Updated files

---
+++ background.js
@@ -0,0 +1,8 @@
+//Define search Button
+let search_btn = document.getElementById("search_btn");
+//Define Search Bar
+let search_bar = document.getElementById("")
+
+search_btn.onclick = function(){
+
+}; 
