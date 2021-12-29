903c3f06914e4a66446092146a02f58e66910703
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2021-12-29 19:21:55 +0100

    Uploaded main files

--- background.js
+++
@@ -1,8 +0,0 @@
-//Define search Button
-let search_btn = document.getElementById("search_btn");
-//Define Search Bar
-let search_bar = document.getElementById("")
-
-search_btn.onclick = function(){
-
-}; 
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import fetch from "node-fetch";
 
-var query = `
+let searchbar = document.getElementById("searchBar").textContent;
+
+searchbar.addEventListener("change", updateValue);
+
+function updateValue(e) {
+  var query = `
 query ($id: Int, $page: Int, $perPage: Int, $search: String) {
   Page (page: $page, perPage: $perPage) {
     pageInfo {
@@ -20,36 +25,36 @@ query ($id: Int, $page: Int, $perPage: Int, $search: String) {
 }
 `;
 
-var variables = {
-  search: "Fate/Zero",
-  page: 1,
-  perPage: 9,
-};
-
-var url = "https://graphql.anilist.co",
-  options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query: query,
-      variables: variables,
-    }),
+  var variables = {
+    search: searchbar,
+    page: 1,
+    perPage: 9,
   };
 
-fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
+  var url = "https://graphql.anilist.co",
+    options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
+    };
 
-function handleResponse(response) {
-  return response.json().then(function (json) {
-    return response.ok ? json : Promise.reject(json);
-  });
-}
-function handleData(data) {
-  console.log(JSON.stringify(data));
-}
-function handleError(error) {
-  
-  console.error(error);
+  fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
+
+  function handleResponse(response) {
+    return response.json().then(function (json) {
+      return response.ok ? json : Promise.reject(json);
+    });
+  }
+  function handleData(data) {
+    console.log(JSON.stringify(data));
+  }
+  function handleError(error) {
+    console.error(error);
+  }
 }
